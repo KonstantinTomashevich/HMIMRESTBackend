f7ad072d12cb43991e143198cbffb8a742dacfa3
Vlad Fishbain <vlad_fb@mail.ru>
2020-11-19 13:44:36 +0300

    Added assert to equal the auth token objects

--- src/test/java/hmim/eteam/rest/backend/repository/user/AuthTokenRepositoryTests.java
+++ src/test/java/hmim/eteam/rest/backend/repository/user/AuthTokenRepositoryTests.java
@@ -37,6 +37,7 @@ public class AuthTokenRepositoryTests {
 
         Optional<AuthToken> authToken = authTokenRepository.resolveToken("id");
         Assert.assertTrue(authToken.isPresent());
+        Assert.assertTrue(firstAuthToken.equals(authToken.get()));
 
     }
 
